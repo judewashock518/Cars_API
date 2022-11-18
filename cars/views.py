c7685271050cd9c025a292f011c5f45b798e78e6
JW <114599894+judewashock518@users.noreply.github.com>
2022-11-18 16:17:53 -0500

    updated serializer errors and data returns for new entries and import frameworks

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,19 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
-@api_view(['GET'])
+from rest_framework import status 
+from .serializers import CarSerializer
+from .models import Car
+@api_view(['GET', 'POST'])
 def cars_list(request):
 
-
-    return Response('ok')
+    if request.method == 'GET':
+        cars = Car.objects.all()
+        serializer = CarSerializer(cars, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = CarSerializer(data=request.data)
+        if serializer.is_valid() == True:
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
